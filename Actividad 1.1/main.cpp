e0608fe8c1517de4c3839cae56137bc4c2959904
Arturo Díaz <93755653+dembA7@users.noreply.github.com>
2023-08-16 19:21:41 -0600

    Initial commit

---
+++ Actividad 1.1/main.cpp
@@ -0,0 +1,20 @@
+// =================================================================
+//
+// File: main.cpp
+//
+// Authors: Arturo Díaz López    - A01709522
+//          Diego Vega Camacho   - 
+//          Ian Padrón Corona    -
+//
+// Description: 
+//
+//
+// To compile: g++ main.cpp -o app
+//
+// =================================================================
+
+#include <iostream>
+#include "shirts.h"
+#include "sodas.h"
+
+using namespace std;
