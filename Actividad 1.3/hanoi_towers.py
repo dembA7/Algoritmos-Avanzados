162ffc72e3c1ecbbe330b0e4e51bcd3a66120d7d
dembo <a01709522@tec.mx>
2023-08-21 08:20:58 -0600

    add total moves count

--- Actividad 1.3/hanoi_towers.py
+++ Actividad 1.3/hanoi_towers.py
@@ -1,18 +1,13 @@
-# Recursive Python function to solve tower of hanoi
-
-
-def TowerOfHanoi(n, from_rod, to_rod, aux_rod):
-	if n == 0:
-		return
-	TowerOfHanoi(n-1, from_rod, aux_rod, to_rod)
-	print("Move disk", n, "from rod", from_rod, "to rod", to_rod)
-	TowerOfHanoi(n-1, aux_rod, to_rod, from_rod)
-
+def TowerOfHanoi(n, from_rod, to_rod, aux_rod, move_count):
+    if n == 0:
+        return
+    TowerOfHanoi(n-1, from_rod, aux_rod, to_rod, move_count)
+    move_count[0] += 1
+    print("Move disk", n, "from rod", from_rod, "to rod", to_rod)
+    TowerOfHanoi(n-1, aux_rod, to_rod, from_rod, move_count)
 
 # Driver code
-N = 3
-
-# A, C, B are the name of rods
-TowerOfHanoi(N, 'A', 'C', 'B')
-
-# Contributed By Harshit Agrawal
+N = int(input("Enter the number of rods: "))
+move_count = [0]  # Counter to store the number of movements
+TowerOfHanoi(N, 'A', 'C', 'B', move_count)
+print("Total movements:", move_count[0])
