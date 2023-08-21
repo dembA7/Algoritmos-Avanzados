c53a66fb2aa76d7d637fe2dbf5234d4d48db5a68
Ian326 <a01708940@tec.mx>
2023-08-21 08:25:19 -0600

    Fixed typo

--- Actividad 1.3/hanoi_towers.py
+++ Actividad 1.3/hanoi_towers.py
@@ -1,13 +1,21 @@
 def TowerOfHanoi(n, from_rod, to_rod, aux_rod, move_count):
+    
     if n == 0:
         return
+    
     TowerOfHanoi(n-1, from_rod, aux_rod, to_rod, move_count)
+    
     move_count[0] += 1
+    
     print("Move disk", n, "from rod", from_rod, "to rod", to_rod)
+    
     TowerOfHanoi(n-1, aux_rod, to_rod, from_rod, move_count)
 
 # Driver code
-N = int(input("Enter the number of rods: "))
+N = int(input("Enter the number of disks: "))
+
 move_count = [0]  # Counter to store the number of movements
+
 TowerOfHanoi(N, 'A', 'C', 'B', move_count)
+
 print("Total movements:", move_count[0])
