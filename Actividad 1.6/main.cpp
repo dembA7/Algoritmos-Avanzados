ad82c9ad8d0043efc9abf2faff6665fda1b06cc0
Diego Vega <diego.vega.camacho@gmail.com>
2023-09-10 20:21:56 -0600

    empezanding

---
+++ Actividad 1.6/main.cpp
@@ -0,0 +1,17 @@
+// ==========================================================================================
+//
+// File: main.cpp
+//
+// Authors: Arturo Díaz López  - A01709522
+//          Diego Vega Camacho - A01704492
+//          Ian Padrón Corona  - A01708940
+//
+// Description: 
+//
+// ==========================================================================================
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
