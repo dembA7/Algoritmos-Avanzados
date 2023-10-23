3f16082fb8c5633bab249dc3a77d9bd9fcf338d6
unknown <a01709522@tec.mx>
2023-10-22 22:30:08 -0600

    fix format

--- Actividad 3.2/main.cpp
+++ Actividad 3.2/main.cpp
@@ -9,6 +9,7 @@
 // Description: Implementación de los algoritmos Djikstra y Floyd
 //
 // ==========================================================================================
+
 #include <iostream>
 #include <vector>
 #include <limits>
@@ -25,22 +26,20 @@ using namespace std;
 // @return: Imprime el arreglo de substrings ordenado alfabéticamente.
 // @complexity O(n)
 // ==========================================================================================
-void dijkstra(vector<vector<int> > &graph, int startNode, vector<int> &distances)
-{
+
+void dijkstra(vector<vector<int> > &graph, int startNode, vector<int> &distances) {
+
   int numNodes = graph.size();
   vector<bool> visited(numNodes, false);
 
   distances.assign(numNodes, INF);
   distances[startNode] = 0;
 
-  for (int i = 0; i < numNodes - 1; i++)
-  {
+  for (int i = 0; i < numNodes - 1; i++) {
     int minDistance = INF, minNode = -1;
 
-    for (int node = 0; node < numNodes; node++)
-    {
-      if (!visited[node] && distances[node] < minDistance)
-      {
+    for (int node = 0; node < numNodes; node++) {
+      if (!visited[node] && distances[node] < minDistance) {
         minNode = node;
         minDistance = distances[node];
       }
@@ -51,13 +50,10 @@ void dijkstra(vector<vector<int> > &graph, int startNode, vector<int> &distances
 
     visited[minNode] = true;
 
-    for (int neighbour = 0; neighbour < numNodes; neighbour++)
-    {
-      if (!visited[neighbour] && graph[minNode][neighbour] != INF)
-      {
+    for (int neighbour = 0; neighbour < numNodes; neighbour++) {
+      if (!visited[neighbour] && graph[minNode][neighbour] != INF) {
         int newDistance = distances[minNode] + graph[minNode][neighbour];
-        if (newDistance < distances[neighbour])
-        {
+        if (newDistance < distances[neighbour]) {
           distances[neighbour] = newDistance;
         }
       }
@@ -73,8 +69,9 @@ void dijkstra(vector<vector<int> > &graph, int startNode, vector<int> &distances
 // @return: Imprime el arreglo de substrings ordenado alfabéticamente.
 // @complexity O(n)
 // ==========================================================================================
-void floyd(vector<vector<int> > &graph)
-{
+
+void floyd(vector<vector<int> > &graph) {
+
   int numNodes = graph.size();
 
   for (int i = 0; i < numNodes; i++)
@@ -93,8 +90,8 @@ void floyd(vector<vector<int> > &graph)
 // @return: Imprime el arreglo de substrings ordenado alfabéticamente.
 // @complexity O(n)
 // ==========================================================================================
-int main()
-{
+
+int main() {
   int graphSize;
 
   cout << "Ingrese el tamaño de su grafo: ";
@@ -105,20 +102,18 @@ int main()
   vector<vector<int> > graph(graphSize, vector<int>(graphSize));
 
   for (int i = 0; i < graphSize; i++)
-    for (int j = 0; j < graphSize; j++)
-    {
+    for (int j = 0; j < graphSize; j++) {
       int buffer;
       cin >> buffer;
       graph[i][j] = buffer == -1 ? INF : buffer;
     }
 
   cout << "Dijkstra results:" << endl;
-  for (int i = 0; i < graphSize; i++)
-  {
+  for (int i = 0; i < graphSize; i++) {
     vector<int> distances;
     dijkstra(graph, i, distances);
-    for (int j = 0; j < distances.size(); j++)
-    {
+
+    for (int j = 0; j < distances.size(); j++) {
       if (j == i)
         continue;
       cout << "From node "
@@ -134,10 +129,8 @@ int main()
   cout << "\nFloyd results:" << endl;
   vector<vector<int> > res(graph);
   floyd(res);
-  for (auto i : res)
-  {
-    for (auto j : i)
-    {
+  for (auto i : res) {
+    for (auto j : i) {
       if (j == INF)
         cout << "No path ";
       else
