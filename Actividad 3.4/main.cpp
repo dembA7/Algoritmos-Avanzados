7a7b9202fdbd66bc8ee20fb2573124277758790f
Diego Vega <diego.vega.camacho@gmail.com>
2023-11-08 11:26:36 -0600

    finished implementation

--- Actividad 3.4/main.cpp
+++ Actividad 3.4/main.cpp
@@ -22,7 +22,7 @@ using namespace std;
 // @params v: Nodo a verificar
 // @params graph: Grafo a verificar
 // @params color: Vector de colores
-// @params c: Color a verificar
+// @params c: Color para colorear el nodo
 //
 // @return: Regresa true si el nodo puede ser pintado con el color c, false en otro caso
 //
@@ -38,37 +38,68 @@ bool isSafe(int v, vector<vector<int> > &graph, vector<int> &color, int c){
 }
 
 // ==========================================================================================
+// Función graphColoringAux, función auxiliar para probar todas las combinaciones de colores
+// 
+// @params graph: Grafo a verificar
+// @params m: Número de colores disponibles
+// @params color: Vector de colores
+// @params v: Nodo a verificar
+//
+// @return: Regresa true si existe una solución para colorear el grafo, false en otro caso
+//
+// @complexity O(n)
+// ==========================================================================================
+bool graphColoringAux(vector<vector<int> >& graph, int m, vector<int>& color, int v){
+    if (v == graph.size())
+        return true;
+
+    for (int c = 1; c <= m; c++){
+        if (isSafe(v, graph, color, c)){
+            color[v] = c;
+            if (graphColoringAux(graph, m, color, v + 1))
+                return true;
+            color[v] = -1;
+        }
+    }
+    return false;
+}
+
+// ==========================================================================================
 // Función graphColoring, función para coordinar la coloración del grafo 
 // 
 // @params graph: Grafo a verificar
 // @params m: Número de colores disponibles
 //
-// @return: 
+// @return: Resultados y estatus de la coloración del grafo
 //
 // @complexity O(n)
 // ==========================================================================================
 
 bool graphColoring(vector<vector<int> >& graph, int m) {
+    vector<int> color(graph.size(), -1);
 
+    if (graphColoringAux(graph, m, color, 0)){
+        cout << "Solución encontrada" << endl;
+        for (int i = 0; i < color.size(); i++){
+            cout << "Vertice " << i << " = Color " << color[i] << endl;
+        }
+        return true;
+    }
+    else{
+        cout << "No existe solución" << endl;
+        return false;
+    }
 }
 
 // ==========================================================================================
-// Función graphColoringAux, función auxiliar para probar todas las combinaciones de colores
-// 
-// @params v: Nodo a verificar
+// Función main, función principal que coordina y ejecuta el programa. Solicita número de 
+// nodos, la matriz de adyacencia y el número de colores. Imprime la solución al problema
+// de colorear un grafo utilizando el algoritmo de fuerza bruta y el algoritmo de 
+// backtracking
 //
-// @return: 
+// @params none
 //
-// @complexity O(n)
-// ==========================================================================================
-bool graphColoringAux(){
-
-}
-
-// ==========================================================================================
-// Función main, función principal que coordina y ejecuta el programa
-//
-// @return: Grafo coloreado
+// @return: 0 si el programa se ejecutó correctamente.
 //
 // @complexity O(n^2)
 // ==========================================================================================
@@ -77,7 +108,7 @@ int main(){
     cout << "Ingrese el número de nodos: ";
     cin >> n;
 
-    vector<vector<int>> graph(n, vector<int>(n));
+    vector<vector<int> > graph(n, vector<int>(n));
     cout << "Ingrese la matriz de adyacencia: " << endl;
     for (int i = 0; i < n; i++){
         for (int j = 0; j < n; j++){
